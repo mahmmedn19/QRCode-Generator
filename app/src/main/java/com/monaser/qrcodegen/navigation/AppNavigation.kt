fc23584b82a9a68b94d3638b5f92cb6aeb6e76f8
Mohamed Naser <mahmmedn19@gmail.com>
2024-06-12 09:01:15 +0300

    enhance navigation transition animated

--- app/src/main/java/com/monaser/qrcodegen/navigation/AppNavigation.kt
+++ app/src/main/java/com/monaser/qrcodegen/navigation/AppNavigation.kt
@@ -3,6 +3,12 @@
  */
 package com.monaser.qrcodegen.navigation
 
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import com.monaser.qrcodegen.ui.screens.payment_options.PaymentOptionsScreen
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -16,7 +22,28 @@ import com.monaser.qrcodegen.ui.screens.user_info.UserFormScreen
 fun AppNavigation() {
     val navController = rememberNavController()
     CompositionLocalProvider(LocalNavigationProvider provides navController) {
-        NavHost(navController, startDestination = Screen.Home.route) {
+        NavHost(
+            navController,
+            startDestination = Screen.Home.route,
+            enterTransition = {
+                slideInHorizontally(initialOffsetX = { 1000 }, animationSpec = tween(700)) + fadeIn(animationSpec = tween(700))
+            },
+            exitTransition = {
+                slideOutHorizontally(targetOffsetX = { -1000 }, animationSpec = tween(700)) + fadeOut(animationSpec = tween(700))
+            },
+            popEnterTransition = {
+                slideInHorizontally(initialOffsetX = { -1000 }, animationSpec = tween(700)) + fadeIn(animationSpec = tween(700))
+            },
+            popExitTransition = {
+                slideOutHorizontally(targetOffsetX = { 1000 }, animationSpec = tween(700)) + fadeOut(animationSpec = tween(700))
+            },
+            sizeTransform = {
+                SizeTransform(
+                    clip = false,
+                    sizeAnimationSpec = { _, _ -> tween(durationMillis = 700) }
+                )
+            }
+        ) {
             composable(Screen.Home.route) {
                 UserFormScreen()
             }
